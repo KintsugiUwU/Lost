0c7ac2b7fb034b645d1665ce3071b1f2b77ef401
KintsugiUwU <134534993+KintsugiUwU@users.noreply.github.com>
2023-06-15 21:00:05 +0200

    Add comments on Mostro.java

--- Mostro.java
+++ Mostro.java
@@ -1,9 +1,9 @@
-public class Mostro extends Personaggio {
+public class Mostro extends Personaggio {  // Costruttore della classe Mostro, che richiama il costruttore della classe Personaggio
     public Mostro(String nome, int vita, int attacco) {
         super(nome, vita, attacco);
-    }
+    } // Metodo che gestisce l'attacco del mostro al personaggio nemico
     @Override
     public void attacca(Personaggio nemico) {
-        nemico.setVita(nemico.getVita() - (random.nextInt(10 - 1 + 1) + 1 + Dungeon.livello));
+        nemico.setVita(nemico.getVita() - (random.nextInt(10 - 1 + 1) + 1 + Dungeon.livello)); // si calcola il danno e si toglie il danno alla vita del personaggio nemico
     }
 }
